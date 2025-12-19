30123daa2126096f2ef4d06c032bcfc146f8d30a
Artem Agafonov <agart@mail.ru>
2025-12-19 23:30:50 +0300

    Add ch02_01.md, ch02_02.md.

---
+++ src/ch02/05_div.pas
@@ -0,0 +1,9 @@
+program divide;
+begin
+    writeln('14 / 7 = ', 14/7);
+    writeln('27 / 4 = ', 27 / 4);
+    writeln('27 div 4 = ', 27 div 4);
+    writeln('27 mod 4 = ', 27 mod 4);
+    writeln();
+    writeln(27 div 4, ' ', 27 mod 4)
+end.
